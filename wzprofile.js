e7f2835dbfd1c080aee5c6007b78aa7336b0baba
Wanadry Zulfikar <wana.dry@gmail.com>
2013-06-12 16:32:45 +0800

    Create wzprofile.js
    Profil

---
+++ wzprofile.js
@@ -0,0 +1,20 @@
+//------------Profil Anda ------------- WZ
+/* <![CDATA[ */
+document.write('<div class="wzdiv">'
++'<ul id="navigation"><a href="#x" id="link-x">X</a>About me</ul>'
++'<center><table cellpadding="10" cellspacing="10"><tbody><tr> '
++'<td align="justify" valign="top" width="150"><img src="'+''+Foto_Profil_Anda+''+'"/></td>'
++'<td align="left" valign="top" width="200">'
++'<span style="color:orange"><b>'+''+Nama_Profil_Anda+''+'</b></span><br/><br/>'
++'<span><b>'+''+Kelamin_Usia_Anda+''+'</b></span><br/><br/>'
++'<img src="http://3.bp.blogspot.com/-hqeakElGvQs/UM07kV4IHvI/AAAAAAAABJ8/ggEB51bLiW4/s1600/icon_Indonesia.gif"/><span style="padding-left:10px"><b>'+''+Kota_Anda_Tinggal +''+'</b></span><br/><br/>'
++'<img src="http://2.bp.blogspot.com/-zfZsVJAP0qA/UM09X7DwjHI/AAAAAAAABKM/ovFt5MjZL4o/s1600/email_icon.gif"/><span style="padding-left:10px">'+''+Pesan_Anda+''+'</span></td>'
++'</tr></tbody></table></center>'
++'<center><table cellpadding="10" cellspacing="10"><tbody><tr>'
++'<a href="'+''+Url_Facebook_Anda+''+'" target="_blank"><img class="START" src="http://1.bp.blogspot.com/-tk9lAx56EHY/UM0v5KVb9MI/AAAAAAAABJU/2Ig338_32j4/s1600/facebook.png" title="Profil Facebook"/></a>'
++'<a href="'+''+Url_Twitter_Anda+''+'" target="_blank"><img class="START" src="http://1.bp.blogspot.com/-oS4iyZ4_9SM/UM0v7FUV65I/AAAAAAAABJc/Aaf0ASSpgsA/s1600/twitter.jpg" title="Profil Twitter"/></a>'
++'<a href="'+''+Url_Google_Anda+''+'" target="_blank"><img class="START" src="http://1.bp.blogspot.com/-KFRlVwlFiwQ/UM0v1UJDF3I/AAAAAAAABJE/8cHoas7aydk/s1600/bj-google-plus.jpg" title="Profil Google"/></a>'
++'<a href="http://www.blogger.com/follow-blog.g?blogID='+''+ID_Blogger_Anda+''+'" target="_blank"><img class="START" src="http://4.bp.blogspot.com/-tOFW1rb7O6E/UM0v2zwvPyI/AAAAAAAABJM/-bl3tzJh7u0/s1600/blogger.jpg" title="Ikuti Blogger Saya"/></a>'
++'</tr></tbody></table></center>');
+document.write('</div>');
+/* ]]> */
